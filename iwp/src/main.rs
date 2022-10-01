3a8cd3f128597b1ba360834d633784a3a032fdb3
luiz-couto <luiz.felipe.556@outlook.com>
2022-10-01 19:51:47 -0300

    get pixel neighbours

--- iwp/src/main.rs
+++ iwp/src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use image::io::Reader as ImageReader;
+use image::{self, Luma};
+use std::error::Error;
+
+pub enum ConnTypes {
+    Four = 4,
+    Eight = 8,
+}
+
+fn get_pixel_neighbours(
+    img: &image::ImageBuffer<Luma<u16>, Vec<u16>>,
+    coords: (u32, u32),
+    conn: ConnTypes,
+) -> Vec<(u32, u32)> {
+    let x = coords.0;
+    let y = coords.1;
+    let mut neighbours = Vec::new();
+
+    let floor_x = if x != 0 { x - 1 } else { x };
+    let floor_y = if y != 0 { y - 1 } else { y };
+
+    for i in (floor_x)..(x + 2) {
+        for j in (floor_y)..(y + 2) {
+            if !(i == x && j == y) {
+                match img.get_pixel_checked(i, j) {
+                    Some(_) => match conn {
+                        ConnTypes::Four => {
+                            if i == x || j == y {
+                                neighbours.push((i, j));
+                            }
+                        }
+
+                        ConnTypes::Eight => neighbours.push((i, j)),
+                    },
+                    None => continue,
+                }
+            }
+        }
+    }
+
+    return neighbours;
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let img = ImageReader::open("fish.png")?.decode()?;
+
+    let img_in_grey = img.to_luma16();
+
+    let dimensions = img_in_grey.dimensions();
+    println!("dimensions: {:?}", dimensions);
+
+    let ngbs = get_pixel_neighbours(&img_in_grey, (7, 0), ConnTypes::Four);
+
+    println!("{:?}", ngbs);
+    //println!("Hello, world!");
+    Ok(())
 }
