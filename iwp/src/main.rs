1ada1eb2b593408fc410890a78f7449d5ec33a8d
luiz-couto <luiz.felipe.556@outlook.com>
2022-10-01 22:57:33 -0300

    get initial pixels

--- iwp/src/main.rs
+++ iwp/src/main.rs
@@ -41,17 +41,117 @@ fn get_pixel_neighbours(
     return neighbours;
 }
 
+fn update_pixel(
+    pixel_coords: (u32, u32),
+    mask: &image::ImageBuffer<Luma<u16>, Vec<u16>>,
+    marker: &mut image::ImageBuffer<Luma<u16>, Vec<u16>>,
+) {
+    let pixel_ngbs = get_pixel_neighbours(marker, pixel_coords, ConnTypes::Eight);
+
+    let pixel = marker.get_pixel(pixel_coords.0, pixel_coords.1);
+    let mut greater = pixel.0[0];
+    for ngb_coord in &pixel_ngbs {
+        let ngb = marker.get_pixel(ngb_coord.0, ngb_coord.1);
+
+        if ngb.0[0] > greater {
+            greater = ngb.0[0];
+        }
+    }
+
+    let mut pixel = marker.get_pixel_mut(pixel_coords.0, pixel_coords.1);
+    let mask_pixel = mask.get_pixel(pixel_coords.0, pixel_coords.1);
+
+    if greater > mask_pixel.0[0] {
+        greater = mask_pixel.0[0];
+    }
+
+    pixel.0[0] = greater;
+}
+
+fn get_initial_pixels(
+    mask: &image::ImageBuffer<Luma<u16>, Vec<u16>>,
+    marker: &mut image::ImageBuffer<Luma<u16>, Vec<u16>>,
+) -> Vec<(u32, u32)> {
+    let width = mask.width();
+    let height = mask.height();
+    let mut queue = Vec::new();
+
+    for i in 0..height {
+        for j in 0..width {
+            let pixel_coords = (j, i);
+            let pixel_ngbs = get_pixel_neighbours(marker, pixel_coords, ConnTypes::Eight);
+
+            let pixel = marker.get_pixel(pixel_coords.0, pixel_coords.1);
+            let mut greater = pixel.0[0];
+            for ngb_coord in &pixel_ngbs {
+                let ngb = marker.get_pixel(ngb_coord.0, ngb_coord.1);
+
+                if ngb.0[0] > greater {
+                    greater = ngb.0[0];
+                }
+            }
+
+            let mut pixel = marker.get_pixel_mut(pixel_coords.0, pixel_coords.1);
+            let mask_pixel = mask.get_pixel(pixel_coords.0, pixel_coords.1);
+
+            if greater > mask_pixel.0[0] {
+                greater = mask_pixel.0[0];
+            }
+
+            pixel.0[0] = greater;
+        }
+    }
+
+    for i in (0..height).rev() {
+        for j in (0..width).rev() {
+            let pixel_coords = (j, i);
+            let pixel_ngbs = get_pixel_neighbours(marker, pixel_coords, ConnTypes::Eight);
+
+            let pixel = marker.get_pixel(pixel_coords.0, pixel_coords.1);
+            let mut greater = pixel.0[0];
+            for ngb_coord in &pixel_ngbs {
+                let ngb = marker.get_pixel(ngb_coord.0, ngb_coord.1);
+
+                if ngb.0[0] > greater {
+                    greater = ngb.0[0];
+                }
+            }
+
+            let mut pixel = marker.get_pixel_mut(pixel_coords.0, pixel_coords.1);
+            let mask_pixel = mask.get_pixel(pixel_coords.0, pixel_coords.1);
+
+            if greater > mask_pixel.0[0] {
+                greater = mask_pixel.0[0];
+            }
+
+            pixel.0[0] = greater;
+            let pixel_value = pixel.0[0];
+
+            ////////////////////
+
+            for ngb_coord in pixel_ngbs {
+                let ngb = marker.get_pixel(ngb_coord.0, ngb_coord.1);
+
+                if (ngb.0[0] < pixel_value) && (ngb.0[0] < mask_pixel.0[0]) {
+                    queue.push(ngb_coord);
+                }
+            }
+        }
+    }
+
+    return queue;
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let img = ImageReader::open("fish.png")?.decode()?;
 
-    let img_in_grey = img.to_luma16();
+    let mask = img.to_luma16();
+    let mut marker = img.to_luma16();
 
-    let dimensions = img_in_grey.dimensions();
+    let dimensions = mask.dimensions();
     println!("dimensions: {:?}", dimensions);
 
-    let ngbs = get_pixel_neighbours(&img_in_grey, (7, 0), ConnTypes::Four);
+    get_initial_pixels(&mask, &mut marker);
 
-    println!("{:?}", ngbs);
-    //println!("Hello, world!");
     Ok(())
 }
