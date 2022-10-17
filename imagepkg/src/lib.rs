1a2fc922a0557ff79956214144e01b90c1b2426f
luiz-couto <luiz.felipe.556@outlook.com>
2022-10-16 23:22:26 -0300

    make dist type pub

--- imagepkg/src/lib.rs
+++ imagepkg/src/lib.rs
@@ -5,6 +5,6 @@ mod img;
 pub mod iwp;
 mod mr;
 
-pub use crate::dist_transform::dist_transform;
+pub use crate::dist_transform::{dist_transform, DistTypes};
 pub use crate::img::{get_pixel_neighbours, ConnTypes, PixelT};
 pub use crate::mr::morph_reconstruction;
