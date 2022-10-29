15825d46bf46bccb86d2c9943dd8338ff1bab6ef
luiz-couto <luiz.felipe.556@outlook.com>
2022-10-29 13:58:49 -0300

    rmse between imgs (comparing result with matlab)

--- imagepkg/tests/morph_recons.rs
+++ imagepkg/tests/morph_recons.rs
@@ -2,6 +2,14 @@ use image;
 use image::io::Reader as ImageReader;
 use imagepkg;
 
+use crate::common::rmse_between_imgs;
+
+mod common;
+
+// Images are considered very smimilar if the mean difference between
+// their pixels are less than 0.5
+const IMG_DIFF_THRESHOLD: f64 = 0.5;
+
 #[test]
 fn test_morphological_reconstruction() {
     let img_mask = ImageReader::open("./tests/imgs/mr/mask.png")
@@ -21,5 +29,14 @@ fn test_morphological_reconstruction() {
 
     imagepkg::morph_reconstruction(&mut mask, &mut marker);
 
-    marker.save("./tests/imgs/mr/result.png").unwrap();
+    let expected = ImageReader::open("./tests/imgs/mr/result_matlab.png")
+        .unwrap()
+        .decode()
+        .unwrap()
+        .to_luma8();
+
+    let rmse = rmse_between_imgs(&expected, &marker);
+
+    assert!(rmse <= IMG_DIFF_THRESHOLD);
+    //marker.save("./tests/imgs/mr/result.png").unwrap();
 }
